e93b8d72b8406d1ccd28ce1a012a3c33c7e4ffe3
Saurabh Awasthi <77627816+GitinitSaurabh@users.noreply.github.com>
2023-11-29 04:13:44 +0530

    Create IRepository.cs

---
+++ IRepository.cs
@@ -0,0 +1,10 @@
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T Get(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
+
+
